f53d10fd2bed89b63fb4ce3f95b389f12af201e7
Logan MacLaren <maclarel@github.com>
2024-02-06 19:54:35 -0500

    updated install script for codespaces

--- install.sh
+++ install.sh
@@ -1,52 +1,32 @@
 #!/bin/bash
-exit 0 # Adding so this doesn't get called by Codespaces while in development
 set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # List only files/config dirs that should be copied
-dotfiles=".vimrc .zshrc .tmux.conf"
-configdirs=".config/i3 .config/i3status .config/gh"
+DOTFILES=".vimrc .zshrc .tmux.conf"
+CONFIGDIRS="gh" # limited list since this is only for codespaces at this time
 
-# Dotfiles
-if [ -f ~/"$f" ]; then
-        echo "Backing up pre-existing $f..."
-        mv ~/"$f" ~/"$f".orig
-fi
+# DOTFILES
+for f in $DOTFILES; do
+        if [ -f ~/"$f" ]; then
+                mkdir -p ~/dotfiles_backups
+                echo "Backing up pre-existing $f..."
+                mv ~/"$f" ~/dotfiles_backups/"$f"
+        fi
 
-for f in $dotfiles; do
-        echo "Copying $f to ~/"
-        cp "$f" ~/"$f"
+        echo "Linking $f"
+        ln -s "$SCRIPT_DIR/$f" ~/"$f"
 done
 
-# Config dirs
-if [ -d ~/.config ]; then
-        echo "Backing up pre-existing ~/.config..."
-        cp -R ~/.config ~/.config.orig
-fi
+# .config dirs
+mkdir -p ~/.config
+for f in $CONFIGDIRS; do
+        if [ -d ~/.config/"$f" ]; then
+                mkdir -p ~/dotfiles_backups
+                echo "Backing up pre-existing $f..."
+                mv ~/.config/"$f" ~/dotfiles_backups/"$f"
+        fi
 
-for f in $configdirs; do
-        echo "Copying $f to ~/"
-        cp -R "$f" ~/"$f"
+        echo "Linking $f"
+        ln -s "$SCRIPT_DIR/.config/$f" ~/.config/
 done
-
-# Vim
-if [ -d ~/.vim ]; then
-        echo "Backing up existing .vim dir"
-        cp -R ~/.vim ~/.vim.old
-        echo "Copying .vim for .vim"
-        cp -R .vim ~/.vim
-fi
-
-# Neovim
-if [ ! -f /usr/bin/nvim ]; then
-	sudo apt update && sudo apt install -y neovim
-fi
-
-if [ ! -f ~/.config/nvim/init.vim ]; then
-	mkdir -p ~/.config/nvim
-	ln -s ~/.vimrc ~/.config/nvim/init.vim
-fi
-
-# zsh plugins
-if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-fi
