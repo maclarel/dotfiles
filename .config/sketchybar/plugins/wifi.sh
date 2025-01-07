65f3175f6042e861cc7852a364051e57171f6f5f
Logan MacLaren <logan.maclaren@gmail.com>
2025-01-07 11:45:04 -0500

    apple broke things again, fixing wifi info

--- .config/sketchybar/plugins/wifi.sh
+++ .config/sketchybar/plugins/wifi.sh
@@ -3,9 +3,7 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 
-# Workaround for missing WiFi information in Sonoma
-INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed "s/Current Wi-Fi Network: //")"
-
+INFO=$(ipconfig getsummary "$(networksetup -listallhardwareports | awk '/Wi-Fi|AirPort/{getline; print $NF}')" | grep '  SSID : ' | awk -F ': ' '{print $2}')
 WIFI=${INFO:-"Not Connected"}
 
-sketchybar --set $NAME label="${WIFI}"
+sketchybar --set $NAME label=$WIFI
